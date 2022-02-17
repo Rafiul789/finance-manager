b29d8a323f604d1b1b8148512b49b1cf1269b989
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-02-17 14:12:26 +0600

    some issue fixed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
-document.getElementById('calculate').addEventListener('click', function() {
+// income and total expenses
+
+function getCost() {
     const totalIncome = document.getElementById('total-income');
+
+
+
+
 
     const rent = document.getElementById('rent');
     const food = document.getElementById('food');
@@ -11,16 +17,29 @@ document.getElementById('calculate').addEventListener('click', function() {
     const totalExpense = document.getElementById('expense');
 
     totalExpense.innerText = totalCost;
-
-
     const balance = document.getElementById('balance');
 
     balance.innerText = totalIncome.value - totalExpense.innerText;
 
 
+}
+
+
+// getting income and total expenses calling function
+
+document.getElementById('calculate').addEventListener('click', function() {
+
+    getCost();
+
+
+
+
 })
 
+
+// savings 
 document.getElementById('savings').addEventListener('click', function() {
+
 
     const savingAmount = document.getElementById('save');
     const totalIncome = document.getElementById('total-income');
